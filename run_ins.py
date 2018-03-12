c66dc5821741500917763df3dbeeb20a4d4c19d3
vladimirsvsv77 <vladimirsvsv77@gmail.com>
2018-03-12 19:10:12 +0300

    added files

---
+++ run_ins.py
@@ -0,0 +1,10 @@
+import os
+
+start_point = 206156037
+step_point = 2000000
+count_instance = 50
+
+for i in range(count_instance):
+    print(i)
+    os.system("nohup python3 start_parsing.py -number "
+        +str(i)+" -start "+ str(start_point - (step_point * i)) + " -end " + str(start_point - step_point - (step_point * i)) + " &")
---
+++ start_parsing.py
@@ -0,0 +1,60 @@
+import re
+import requests
+import argparse
+from pymongo import MongoClient
+
+
+parser = argparse.ArgumentParser(description='start_parsing.py')
+parser.add_argument('-number')
+parser.add_argument('-start')
+parser.add_argument('-end')
+opt = parser.parse_args()
+number = opt.number
+start = opt.start
+end = opt.end
+
+
+def write_file(filename, text):
+    f = open(filename, 'a')
+    f.write(text + '\n')
+    f.close()
+
+
+client = MongoClient('localhost', 27017)
+db = client.questions
+questions = db.questions
+
+def start_parsing(count_start, count_end):
+    while count_start > count_end:
+        url = 'https://otvet.mail.ru/api/v2/question'
+        data = {
+            'qid': str(count_start)
+        }
+        req = requests.post(url, data=data)
+
+        try:
+            if req.json()['status'] == '200':
+                if req.json()['bestanswer'] != '0':
+                    question =  req.json()['qtext']
+                    question = re.sub('<[^<]+?>', '', question.replace('\n', ' ').replace('\r', ' '))
+                    answ = req.json()['best']['atext']
+                    answ = re.sub('<[^<]+?>', '', answ.replace('\n', ' ').replace('\r', ' '))
+                    if question != '' and answ != '' and answ is not None and question is not None:
+                        qa = {
+                            "qid": str(count_start),
+                            "question": question,
+                            "answer": answ
+                        }
+                        questions.insert_one(qa)
+
+        except Exception as e:
+            write_file('errors.txt', str(number) + ' - ' + str(e))
+            write_file('errors.txt', str(number) + ' - ' + req.text)
+            write_file('errors.txt', str(number) + ' - ' + '____________________________________')
+
+        if count_start % 1000 == 0:
+            write_file('count_start.txt', str(number) + ' - ' + str(count_start))
+
+        count_start -= 1
+
+start_parsing(int(start), int(end))
