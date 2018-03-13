2d52be0bb832b283f9635f787470499f9ba7b1bc
vladimirsvsv77 <vladimirsvsv77@gmail.com>
2018-03-13 11:58:42 +0300

    change db

--- start_parsing.py
+++ start_parsing.py
@@ -2,6 +2,7 @@ import os
 import re
 import requests
 import argparse
+import time
 from pymongo import MongoClient
 
 
@@ -22,40 +23,56 @@ def write_file(filename, text):
 
 
 client = MongoClient('localhost', 27017)
-db = client.questions
-questions = db.questions
+db = client.mairudata
+questions = db.mairudata
+
 
 def start_parsing(count_start, count_end):
-    while count_start > count_end:
-        url = 'https://otvet.mail.ru/api/v2/question'
-        data = {
-            'qid': str(count_start)
-        }
-        req = requests.post(url, data=data)
+    count_attempt = 0
 
+    while True:
         try:
-            if req.json()['status'] == '200':
-                if req.json()['bestanswer'] != '0':
-                    question =  req.json()['qtext']
-                    question = re.sub('<[^<]+?>', '', question.replace('\n', ' ').replace('\r', ' '))
-                    answ = req.json()['best']['atext']
-                    answ = re.sub('<[^<]+?>', '', answ.replace('\n', ' ').replace('\r', ' '))
-                    if question != '' and answ != '' and answ is not None and question is not None:
-                        qa = {
-                            "qid": str(count_start),
-                            "question": question,
-                            "answer": answ
-                        }
-                        questions.insert_one(qa)
+            while count_start > count_end:
+                url = 'https://otvet.mail.ru/api/v2/question'
+                data = {
+                    'qid': str(count_start)
+                }
+                req = requests.post(url, data=data)
 
-        except Exception as e:
-            write_file('errors.txt', str(number) + ' - ' + str(e))
-            write_file('errors.txt', str(number) + ' - ' + req.text)
-            write_file('errors.txt', str(number) + ' - ' + '____________________________________')
+                try:
+                    if req.json()['status'] == '200':
+                        if req.json()['bestanswer'] != '0':
+                            question =  req.json()['qtext']
+                            question = re.sub('<[^<]+?>', '', question.replace('\n', ' ').replace('\r', ' '))
+                            answ = req.json()['best']['atext']
+                            answ = re.sub('<[^<]+?>', '', answ.replace('\n', ' ').replace('\r', ' '))
+                            if question != '' and answ != '' and answ is not None and question is not None:
+                                qa = {
+                                    "qid": str(count_start),
+                                    "question": question,
+                                    "answer": answ
+                                }
+                                questions.insert_one(qa)
 
-        if count_start % 1000 == 0:
-            write_file('count_start.txt', str(number) + ' - ' + str(count_start))
+                except Exception as e:
+                    write_file('errors.txt', str(number) + ' - ' + str(e))
+                    write_file('errors.txt', str(number) + ' - ' + req.text)
+                    write_file('errors.txt', str(number) + ' - ' + '____________________________________')
 
-        count_start -= 1
+                if count_start % 1000 == 0:
+                    write_file('count_start.txt', str(number) + ' - ' + str(count_start))
+
+                count_start -= 1
+        except:
+            if count_attempt > 2:
+                time.sleep(180)
+                count_start -= 1
+                count_attempt = 0
+            else:
+                time.sleep(60)
+                count_attempt += 1
+
+
+
 
 start_parsing(int(start), int(end))
